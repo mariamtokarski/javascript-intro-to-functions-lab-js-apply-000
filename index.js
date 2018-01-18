18e443d8b85bc7a4b0e140cd922abc7ace892714
Mariam Tokarski <mariamtokarski@gmail.com>
2018-01-18 06:35:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,9 @@ function sayHiToGrandma(string){
 }
 else if (string === string.toUpperCase()){
     return "YES INDEED!"
-}
-else if (string === "I love you, Grandma") {
-  return "I love you, too."
 }}
+
+function sayHiToGrandma(string){
+    if (string === "I love you, Grandma")
+  return "I love you, too."
+}
