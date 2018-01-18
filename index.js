91ae2df83ab1cacf29c4f13b37548c951d70f5d2
Mariam Tokarski <mariamtokarski@gmail.com>
2018-01-18 06:35:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,16 +15,12 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if (string === string.toLowerCase())
-    return "I can\'t hear you!"
+  if (string === string.toLowerCase()){
+    return "I can't hear you!"
 }
-
-function sayHiToGrandma(string){
-  if (string === string.toUpperCase())
+else if (string === string.toUpperCase()){
     return "YES INDEED!"
 }
-
-function sayHiToGrandma(string){
-  if (string === "I love you, Grandma.")
+else if (string === "I love you, Grandma") {
   return "I love you, too."
-}
+}}
