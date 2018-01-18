2a572ef05fa5fdbddebe2b3c75495c02f48d46d5
Mariam Tokarski <mariamtokarski@gmail.com>
2018-01-18 06:27:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,13 +3,28 @@ function shout(string){
 }
 
 function whisper(string){
-  return string.toLowerCase
+  return string.toLowerCase()
 }
 
 function logShout(string){
-  console.log(string.toUpperCase)
+  console.log(string.toUpperCase())
 }
 
 function logWhisper(string){
-  console.log(string.toLowerCase)
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string){
+  if (string === string.toLowerCase())
+    return "I can't hear you!"
+}
+
+function sayHiToGrandma(string){
+  if (string === string.toUpperCase())
+    return "YES INDEED!"
+}
+
+function sayHiToGrandma(string){
+  if (string === "I love you, Grandma.")
+  return "I love you, too."
 }
